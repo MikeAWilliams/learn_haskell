388039a2af1a62ffa5471b008ad84e107056fb0e
MikeAWilliams <mumakin@gmail.com>
2020-03-01 16:34:54 -0700

    more lists

--- baby.hs
+++ baby.hs
@@ -11,3 +11,6 @@ helloWorld = "hello" ++ ' ':"world"
 h = helloWorld!!0
 
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+rightTriangles = [(a,b,c) | c <- [1..10] , b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24]
--- sigmundHomework.hs
+++ sigmundHomework.hs
@@ -3,3 +3,7 @@ import Data.List
 sigmundOne inSet = [item*32+30 | item <- inSet, item*32+30 < 3000]
 sigmundTwo inSet = [item*58+44 | item <- inSet, item*58+44 < 3000]
 sigmundSolve inSet = intersect (sigmundOne inSet) (sigmundTwo inSet)
+
+sigOne inSet = [item * 32 + 30 | item <- inSet]
+sigTwo inSet = [item * 58 + 44 | item <- inSet]
+sigSolve inSet max = intersect [x | x <-(sigOne inSet), x < max] [y | y <-(sigTwo inSet), y< max]
