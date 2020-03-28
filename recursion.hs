4ad1657153bf73b8fbbe648d32fe5f24b9d0ef5e
MikeAWilliams <mumakin@gmail.com>
2020-03-28 15:12:11 -0600

    add recursive example

---
+++ recursion.hs
@@ -0,0 +1,12 @@
+-- directly lifted from learnyouahaskell.com/resucrion
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
+
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs) = max x (maximum' xs)  
