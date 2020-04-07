ab14bbefc88cee17980b7fe444469f4a212fca9c
MikeAWilliams <mumakin@gmail.com>
2020-04-06 22:27:51 -0600

    more io examples

--- inout.hs
+++ inout.hs
@@ -2,3 +2,5 @@ main = do
     putStrLn "Hello, what's your name?"
     name <- getLine
     putStrLn ("Hey " ++ name ++ ", you rock!")
+    putStrLn "Enter some other text and it will be the final value of the program"
+    getLine
