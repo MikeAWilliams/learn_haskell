4ac5c333ced7be712033c559f27c9601801b3f01
MikeAWilliams <mumakin@gmail.com>
2020-04-12 21:40:01 -0600

    finish calc

--- reversePolish.hs
+++ reversePolish.hs
@@ -1,4 +1,7 @@
-reversePolish :: (Num a, Read a) => String -> a
+reversePolish :: (Fractional a, Read a) => String -> a
 reversePolish expression = head (foldl partialCalc [] (words expression))
     where partialCalc (one:two:stack) "*" = (one * two):stack
+          partialCalc (one:two:stack) "/" = (two / one):stack
+          partialCalc (one:two:stack) "+" = (one + two):stack
+          partialCalc (one:two:stack) "-" = (two - one):stack
           partialCalc stack number = read number:stack
