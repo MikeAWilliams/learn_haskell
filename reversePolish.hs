d77efcbb0fa7dcdfdedeb0e71463c11b875a77bc
MikeAWilliams <mumakin@gmail.com>
2020-04-20 06:41:38 -0600

    time reversePolish

--- reversePolish.hs
+++ reversePolish.hs
@@ -5,3 +5,10 @@ reversePolish expression = head (foldl partialCalc [] (words expression))
           partialCalc (one:two:stack) "+" = (one + two):stack
           partialCalc (one:two:stack) "-" = (two - one):stack
           partialCalc stack number = read number:stack
+
+twoList = replicate 1000000 " 2 + "
+twos = concat twoList
+testString = "2 2 +" ++ twos 
+-- :set +s
+-- reversePoliish testString
+-- (3.45 secs, 5,195,872,968 bytes)
